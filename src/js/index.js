ef4c8828aeb8de1610a664edd274ff766bd2843e
Pat <alfonsopatrisha@gmail.com>
2023-05-21 18:39:23 -0700

    Catch and display API error

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 import '../style/style.scss';
 
+// Weather object
 let weather = {
     "apiKey": "754f57af387718693ecfb4a18e6c8a57",
     fetchWeather: function (city) {
@@ -9,9 +10,13 @@ let weather = {
             + "&units=metric&appid=" 
             + this.apiKey
         ).then((response) => response.json())
-        .then((data) => this.displayWeather(data));
+        .then((data) => this.displayWeather(data))
+        .catch((error) => this.displayError())
+
     },
     displayWeather: function(data) {
+        document.querySelector(".not-found").style.display = "none";
+        document.querySelector(".weather").style.display = "block";
         const {name} = data;
         const {icon, description} = data.weather[0];
         const {temp, humidity} = data.main;
@@ -19,11 +24,18 @@ let weather = {
         document.querySelector(".city").innerText = name;
         document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
         document.querySelector(".temp-num").innerText = temp;
+        document.querySelector(".temp-unit"). innerText = "°C";
         document.querySelector(".humidity").innerText = humidity + "%";
         document.querySelector(".description").innerText = description;
         document.querySelector(".wind").innerText = speed + " km/h";
         document.querySelector(".weather").classList.remove("loading");
         document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + description + "')"
+    },
+    displayError: function() {
+        const invalidCity = document.querySelector('#search-bar').value;
+        document.querySelector(".not-found").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+        document.querySelector('.invalid').innerText = `"${invalidCity}" is an invalid location. Try again.`;
     },
     search: function () {
         this.fetchWeather(document.querySelector("#search-bar").value);
@@ -44,6 +56,7 @@ document.querySelector("#search-bar").addEventListener("keyup", function(event) 
 
 weather.fetchWeather("Denver");
 
+// Celsius and Farenheight Toggle Coverter
 document.querySelector(".temp").addEventListener("click", function () {
     let tempUnit = document.querySelector(".temp-unit").innerText;
     let tempNum = document.querySelector(".temp-num").innerText;
